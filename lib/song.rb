af5f0c614f5ec868f5f8587cf03a98c07754b792
Nikole Hache <nh131215@gmail.com>
2019-07-17 18:05:12 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song 
+
+    attr_accessor :name, :artist, :genre
+
+    def initialize(name, artist, genre)
+        @name = name.Array
+        @artist = artist.Array
+        @genre = genre.Array
+        @@count = 0
+
+    def count
+        @name.each do 
+            @@count += 1
+    end
+
+    def artist
+        @@artists = []
+    end
+
+    def genres ()
+        @@genres = []
+
+
+    end
+
+    def genre_count
+
+    end
+
+    def artist_count
+
+    end
+
+end
